59bf2a73c7b4183b352530d91cad0c40f4546259
Srimanta Bagchi <sbagchi@loyalty.com>
2020-10-05 15:33:53 -0400

    Changed some other files to hooks

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
-import { connect } from 'react-redux'
+import React from "react";
+import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
+import { useSelector } from "react-redux";
 
-const Navbar = (props) => {
-  const { auth, profile } = props;
-  // console.log(auth);
-  const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />;
+const Navbar = () => {
+  const auth = useSelector((state) => state.firebase.auth);
+  const profile = useSelector((state) => state.firebase.profile);
+  const links = auth.uid ? (
+    <SignedInLinks profile={profile} />
+  ) : (
+    <SignedOutLinks />
+  );
 
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to='/' className="brand-logo">MarioPlan</Link>
+        <Link to="/" className="brand-logo">
+          MarioPlan
+        </Link>
         {links}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
-  // console.log(state);
-  return{
-    auth: state.firebase.auth,
-    profile: state.firebase.profile
-  }
-}
-
-export default connect(mapStateToProps)(Navbar)
+export default Navbar;
--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { createProject } from '../../store/actions/projectActions'
-import { Redirect } from 'react-router-dom'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
+import { Redirect, useHistory } from "react-router-dom";
 
-class CreateProject extends Component {
-  state = {
-    title: '',
-    content: ''
-  }
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
-  handleSubmit = (e) => {
+const CreateProject = () => {
+  const auth = useSelector((state) => state.firebase.auth);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [project, setProject] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(this.state);
-    this.props.createProject(this.state);
-    this.props.history.push('/');
-  }
-  render() {
-    const { auth } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' /> 
-    return (
-      <div className="container">
-        <form className="white" onSubmit={this.handleSubmit}>
-          <h5 className="grey-text text-darken-3">Create a New Project</h5>
-          <div className="input-field">
-            <input type="text" id='title' onChange={this.handleChange} />
-            <label htmlFor="title">Project Title</label>
-          </div>
-          <div className="input-field">
-            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
-            <label htmlFor="content">Project Content</label>
-          </div>
-          <div className="input-field">
-            <button className="btn pink lighten-1">Create</button>
-          </div>
-        </form>
-      </div>
-    )
-  }
-}
+    dispatch(createProject(project));
+    history.push("/");
+  };
+  if (!auth.uid) return <Redirect to="/signin" />;
+  return (
+    <div className="container">
+      <form className="white" onSubmit={handleSubmit}>
+        <h5 className="grey-text text-darken-3">Create a New Project</h5>
+        <div className="input-field">
+          <input
+            type="text"
+            id="title"
+            onChange={(e) => setProject({ ...project, title: e.target.value })}
+          />
+          <label htmlFor="title">Project Title</label>
+        </div>
+        <div className="input-field">
+          <textarea
+            id="content"
+            className="materialize-textarea"
+            onChange={(e) =>
+              setProject({ ...project, content: e.target.value })
+            }
+          ></textarea>
+          <label htmlFor="content">Project Content</label>
+        </div>
+        <div className="input-field">
+          <button className="btn pink lighten-1">Create</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    auth: state.firebase.auth
-  }
-}
+    createProject: (project) => dispatch(createProject(project)),
+  };
+};
 
-const mapDispatchToProps = dispatch => {
-  return {
-    createProject: (project) => dispatch(createProject(project))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CreateProject)
+export default CreateProject;
