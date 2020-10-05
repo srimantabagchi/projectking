7f6d97b0b1849d8fe4c60a6969e857258e09d2f0
Srimanta Bagchi <sbagchi@loyalty.com>
2020-10-05 15:58:06 -0400

    Changed to hooks

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './store/reducers/rootReducer'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { createFirestoreInstance } from 'redux-firestore';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-import firebase from './config/firebase'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createFirestoreInstance } from "redux-firestore";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import firebase from "./config/firebase";
 
-const store = createStore(rootReducer,
-  compose(
-    applyMiddleware(thunk)
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const rrfConfig = {
-  userProfile: 'users',
-  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  userProfile: "users",
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
   // enableClaims: true // Get custom claims along with the profile
-}
+};
 
 ReactDOM.render(
   <Provider store={store}>
-    <ReactReduxFirebaseProvider firebase={firebase}
-     config={rrfConfig}
-     dispatch={store.dispatch}
-     createFirestoreInstance={createFirestoreInstance}>
+    <ReactReduxFirebaseProvider
+      firebase={firebase}
+      config={rrfConfig}
+      dispatch={store.dispatch}
+      createFirestoreInstance={createFirestoreInstance}
+    >
       <App />
     </ReactReduxFirebaseProvider>
-  </Provider>, 
-  document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
