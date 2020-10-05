33db13f3a7f99841fe2af90fbc2b993782e7e5b9
Srimanta Bagchi <sbagchi@loyalty.com>
2020-10-05 16:03:40 -0400

    Rename repo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          MarioPlan
+          ProjectKing
         </Link>
         {links}
       </div>
